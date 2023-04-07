1df8261bd147a17cbc8bf383c78e5deda56161a9
MauMu21 <miaut16umf@gmail.com>
2023-04-06 22:23:01 -0400

    Conversiones y operaciones básicas

---
+++ Curso/Numeros_Operaciones.py
@@ -0,0 +1,24 @@
+entero = 23
+decimal = 31.78
+complejo = 12 + 5j
+#booleano = True
+#el numeral solo sirve para comentar una sola linea
+
+#las """ sirven para comentar secciones mas grandes
+"""
+print(entero)
+print(decimal)
+print(complejo)
+print(booleano)
+"""
+
+num1 = 20
+num2 = 4
+print("Suma:", (num1 + num2))
+print("Resta:", (num1 - num2))
+print("Multiplicacion:", (num1 * num2))
+print("Division:", (num1 / num2))
+print("Division Exacta:", (num1 // num2))
+print("Potencia", (num1 ** num2))
+
+
